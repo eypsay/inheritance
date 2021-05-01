9e9bcbbcca0825334c180612e6252d11ddce5be5
eyp <eypsay@yahoo.com>
2021-05-01 22:23:29 +0300

    eklendi

--- inheritance2/src/inheritance2/Logger.java
+++ inheritance2/src/inheritance2/Logger.java
@@ -3,6 +3,7 @@ package inheritance2;
 // Bu temel sýnýftýr()base sýnýf
 public class Logger {
 	public void log() {
-System.out.println("Ortak configurasyon");
+		System.out.println("Ortak configurasyon");
 	}
 }
+//bir sýnýf birden fazla interface implemete edebilir.
